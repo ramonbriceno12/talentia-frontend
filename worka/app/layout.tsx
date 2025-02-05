54eb7b45719c30c76b9e3faccb1ab0571f5d7995
Ramón Briceño <ramon.briceno@goliiive.com>
2025-02-05 16:59:05 -0400

    Head

--- worka/app/layout.tsx
+++ worka/app/layout.tsx
@@ -42,18 +42,17 @@ export default function RootLayout({
         <meta name="robots" content="index, follow" />
         <meta name="author" content="Talentia" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta property="og:image" content="https://talentiave.com/img/preview-image.png" />
+        <meta property="og:image" content="https://talentiave.com/img/LOGO-04.png" />
         <meta property="og:image:width" content="1200" />
         <meta property="og:image:height" content="630" />
         <meta property="og:title" content="Talentia - Encuentra Candidatos de Alta Calidad" />
         <meta property="og:description" content="Nuestra IA conecta tu empresa con los mejores talentos, pre-evaluados y listos para trabajar en tu equipo." />
         <meta property="og:url" content="https://talentiave.com/" />
         <meta property="og:type" content="website"/>
-        <meta name="twitter:image" content="https://talentiave.com/img/preview-image.png" />
+        <meta name="twitter:image" content="https://talentiave.com/img/LOGO-04.png" />
         <meta name="twitter:title" content="Talentia - Encuentra Candidatos de Alta Calidad" />
         <meta name="twitter:description" content="Nuestra IA conecta tu empresa con los mejores talentos, pre-evaluados y listos para trabajar en tu equipo." />
         <meta name="twitter:card" content="summary_large_image" />
-        
         <link rel="icon" href="/img/LOGO-03.png" />
 
       </head>
